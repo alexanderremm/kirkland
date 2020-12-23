c14bfea64224298ddea505bd3c973a5d93a4073f
remman <remman@mail.uc.edu>
2020-12-22 22:01:15 -0500

    Base walking animation for the kuckland

--- scripts/Kirkland.gd
+++ scripts/Kirkland.gd
@@ -11,14 +11,37 @@ func _ready():
 	
 func _physics_process(delta):
 	velocity = Vector2()
+	$body.play()
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -WALK_SPEED
+		# adjust animation/head
+		$body.animation = 'right_left'
+		$body.flip_h = true
+		$head.frame = 3
 	if Input.is_action_pressed("ui_right"):
 		velocity.x =  WALK_SPEED
+		# adjust animation/head
+		$body.animation = 'right_left'
+		$body.flip_h = false
+		$head.frame = 2
 	if Input.is_action_pressed("ui_up"):
 		velocity.y = -WALK_SPEED
+		# adjust animation/head
+		$body.animation = 'front_back'
+		$head.frame = 1
 	if Input.is_action_pressed("ui_down"):
 		velocity.y =  WALK_SPEED
+		# adjust animation/head
+		$body.animation = 'front_back'
+		$head.frame = 0
+		
+	# Don't play an animation if he is not moving
+	if velocity == Vector2(0, 0):
+		$body.stop()
+		$body.animation = 'front_back'
+		$body.frame = 2
+		$head.frame = 0
+	
 	move_and_slide(velocity, Vector2(0, 0))
 
 
