a5cc5caeab1de7da7d3b0c3b1d5be3657e8cd72b
remman <remman@mail.uc.edu>
2020-12-27 21:29:37 -0500

    Fixed Adam walking diagonally animation

--- scripts/Kirkland.gd
+++ scripts/Kirkland.gd
@@ -13,36 +13,39 @@ func _physics_process(delta):
 	velocity = Vector2()
 	$body.play()
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = -WALK_SPEED
-		# adjust animation/head
-		$body.animation = 'right_left'
-		$body.flip_h = true
-		$head.frame = 3
+		velocity.x = -1
 	if Input.is_action_pressed("ui_right"):
-		velocity.x =  WALK_SPEED
-		# adjust animation/head
-		$body.animation = 'right_left'
-		$body.flip_h = false
-		$head.frame = 2
+		velocity.x = 1
 	if Input.is_action_pressed("ui_up"):
-		velocity.y = -WALK_SPEED
+		velocity.y = -1
 		# adjust animation/head
 		$body.animation = 'front_back'
+		$body.play('front_back', true)
 		$head.frame = 1
 	if Input.is_action_pressed("ui_down"):
-		velocity.y =  WALK_SPEED
+		velocity.y = 1
 		# adjust animation/head
 		$body.animation = 'front_back'
 		$head.frame = 0
 		
-	# Don't play an animation if he is not moving
-	if velocity == Vector2(0, 0):
-		$body.stop()
-		$body.animation = 'front_back'
-		$body.frame = 2
-		$head.frame = 0
+	# Animation control
+	match velocity:
+		# Don't play an animation if he is not moving
+		Vector2(0, 0):
+			$body.stop()
+			$body.animation = 'front_back'
+			$body.frame = 2
+			$head.frame = 0
+		Vector2(1, 0):
+			$body.animation = 'right_left'
+			$body.flip_h = false
+			$head.frame = 2
+		Vector2(-1, 0):
+			$body.animation = 'right_left'
+			$body.flip_h = true
+			$head.frame = 3
 	
-	move_and_slide(velocity, Vector2(0, 0))
+	move_and_slide(WALK_SPEED * velocity, Vector2(0, 0))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
